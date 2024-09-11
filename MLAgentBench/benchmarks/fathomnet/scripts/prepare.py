2a04f986052ee5208a74fe813927d4230c932ae6
Ning Li <2212363918@qq.com>
2024-09-11 06:23:02 +0000

    fix download path

--- MLAgentBench/benchmarks/fathomnet/scripts/prepare.py
+++ MLAgentBench/benchmarks/fathomnet/scripts/prepare.py
@@ -3,8 +3,7 @@ import pandas as pd
 import json
 
 taskname = "fathomnet-out-of-sample-detection"
-# download_dir = f"benchmarks/{taskname}/env"
-download_dir = "benchmarks/" + taskname + "/env"
+download_dir = "../env"
 
 input(f"Consent to the competition at https://www.kaggle.com/competitions/{taskname}/data; Press any key after you have accepted the rules online.")
 
--- MLAgentBench/benchmarks/feedback/scripts/prepare.py
+++ MLAgentBench/benchmarks/feedback/scripts/prepare.py
@@ -2,8 +2,7 @@ import subprocess
 import pandas as pd
 
 taskname = "feedback-prize-english-language-learning"
-# download_dir = f"benchmarks/{taskname}/env"
-download_dir = "benchmarks/" + taskname + "/env"
+download_dir = "../env"
 
 input(f"Consent to the competition at https://www.kaggle.com/competitions/{taskname}/data; Press any key after you have accepted the rules online.")
 
